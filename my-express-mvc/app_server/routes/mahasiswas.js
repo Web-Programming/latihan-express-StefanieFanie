d1a25a73bee87b2e3ff44ea52e263c2c6835b31f
Stefanie <stefanie_2226240009@mhs.mdp.ac.id>
2024-10-18 18:33:04 +0700

    ADD : data model

---
+++ my-express-mvc/app_server/routes/mahasiswas.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const mahasiswaController = require("../controllers/controllerMahasiswa");
+
+// Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data dalam Database MongoDB
+router.get("/mahasiswa", mahasiswaController.Index);
+module.exports = router;
