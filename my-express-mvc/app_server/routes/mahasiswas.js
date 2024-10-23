10d030f180f6bb135a54e5c4c3a4fd1fb6324eb6
StefanieFanie <stefanie_2226240009@mhs.mdp.ac.id>
2024-10-23 20:26:49 +0700

    ADD : insert data

--- my-express-mvc/app_server/routes/mahasiswas.js
+++ my-express-mvc/app_server/routes/mahasiswas.js
@@ -3,5 +3,9 @@ const router = express.Router();
 const mahasiswaController = require("../controllers/controllerMahasiswa");
 
 // Fungsi dan Rute Index Kita Gunakan Untuk Memanggil Semua Data dalam Database MongoDB
-router.get("/mahasiswa", mahasiswaController.Index);
+router.get("/mahasiswa", mahasiswaController.index);
+router.post("/insert", mahasiswaController.insert); //insert mahasiswa 
+router.patch("/update/:id", mahasiswaController.update); //mengupdate mahasiswa 
+router.get("/show/:id", mahasiswaController.show); //show detail mahasiswa by id 
+router.delete("/delete/:id", mahasiswaController.destroy); //delete mahasiswa by id
 module.exports = router;
